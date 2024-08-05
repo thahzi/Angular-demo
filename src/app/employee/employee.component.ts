001cf1d7fbe637f7ec0e517e57a90a934eaf0a87
thahzi <thahzeebmohamed@gmail.com>
2024-08-05 18:23:38 +0530

    aug5

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent {
+  employee: string="maxwell"
+
+  empImg: string="https://i.pinimg.com/736x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg"
+
+  getDesg(event:any){
+    console.log(event.target.value)
+  }
+}
